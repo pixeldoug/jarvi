93238201e6838879dd65fac32a4d27aa2390dfee
pixeldoug <doughenrique93@gmail.com>
2025-10-05 20:05:31 -0300

    🔧 Adicionar PATCH ao CORS

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -45,7 +45,7 @@ app.use(helmet({
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://jarvi.life');
   res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
   
   if (req.method === 'OPTIONS') {
